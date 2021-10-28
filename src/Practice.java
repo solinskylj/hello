e216113b6f3cbf30936b4564b02364957805911f
LJ Solinsky <solinskylj@gmail.com>
2021-10-28 16:09:19 -0400

    Array as list practice

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello Github");
+
+        List<Integer> allGrades = Arrays.asList(5, 3, 2, 6, 3);
+        int [] grades = new int[allGrades.size()];
+
+        for(int i = 0; i < allGrades.size(); i++) {
+            grades[i] = allGrades.get(i);
+        }
+
+        System.out.println(Arrays.toString(grades));
     }
 }
